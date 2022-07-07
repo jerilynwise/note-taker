2c93146a2b72a2d71309dc0c532bb6aedb0bd827
Jerilyn <jerilyn.wise@paddywax.com>
2022-07-07 12:16:24 -0500

    inital files

---
+++ server.js
@@ -0,0 +1,28 @@
+// required packages
+const express = require('express');
+// calls the server
+const app = express();
+
+// pathway needed to hold the notes database
+const { notes } = require('./db/db.json');
+
+// port to run through Heroku
+const PORT = process.env.PORT || 3001;
+
+
+
+
+
+// gets the information request and requirements
+app.get("/api/notes", (req,res) => {
+    let results = notes;
+    if(req.query) {
+        results = filterByQuery(req.query, results)
+    }
+    res.json(results);
+});
+
+// makes the server listen
+app.listen(PORT, () => {
+    console.log (`API is now on port ${PORT}!`)
+})
